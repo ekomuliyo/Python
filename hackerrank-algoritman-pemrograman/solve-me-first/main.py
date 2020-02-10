87240594180e7085a990883ccf34b0f5c2d2469e
eko <ekomuliyo.com>
2020-02-11 06:55:32 +0700

    solve-me-first

---
+++ hackerrank-algoritman-pemrograman/solve-me-first/main.py
@@ -0,0 +1,12 @@
+
+# masalah : menjumlahkan 2 variabel dalam fungsi
+
+def solveMeFirst(a, b):
+    
+    # solusi : tambahkan return a + b 
+    return a + b
+
+number1 = int(input())
+number2 = int(input())
+result = solveMeFirst(number1, number2)
+print(result)
