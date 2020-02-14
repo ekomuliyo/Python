2a4c637e3cf57e914e7b61f0ae3ee8f9f901c4c8
eko <ekomuliyo.com>
2020-02-14 08:13:06 +0700

    clean input

--- hackerrank-algoritman-pemrograman/simple-array-sum/main.py
+++ hackerrank-algoritman-pemrograman/simple-array-sum/main.py
@@ -23,8 +23,6 @@ def simpleArraySum(ar):
 if __name__ == "__main__":
     fptr = sys.stdout
 
-    ar_count = int(input())
-
     ar = list(map(int, input().rstrip().split()))
     
     result = simpleArraySum(ar)
