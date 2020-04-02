6b453e6f23d0078316fd2f1dab35e84fc90e269e
eko muliyo <ekomuliyo@gmail.com>
2020-04-02 16:29:28 +0700

    slicing-list-and-string

---
+++ pythonic/slicing-list-and-string/main.py
@@ -0,0 +1,12 @@
+# Slicing List
+listmu = [1, 2, 3, 4, 6, 7, 8, 9, 10]
+listA = listmu[2:6] # mengambil nilai dari index ke 2 sampai index 6 - 1
+listB = listmu[3:] # mengambil nilai mulai dari index 3 sampai seterusnya
+listC = listmu[:3] # mengambil nilai dari index pertama sampai index ke 3
+print(f'listku {listA}')
+print(f'listku {listB}')
+print(f'listku {listC}')
+
+# Slicing String
+kata = 'BELAJAR'
+print(kata[0:4])
